95d5f735b46a2475cfe21aa92041fb9e854a45f5
Bradley Collins <bradcollins@advantagovernmentservices.com>
2024-05-03 20:05:17 -0400

    Added Comments and Process setps to the scritps

--- backup_wow.bat
+++ backup_wow.bat
@@ -7,14 +7,30 @@ SET interfaceDir="C:\World of Warcraft\_retail_\Interface"
 SET tempDir="D:\WoWBackUp\%timestamp%"
 SET destDir="\\TOWER\NasBackup\WoWAddonBackup"
 
-mkdir %tempdir%
-robocopy %interfaceDir% "%tempdir%\Interface" /NFL /MIR /MT:32
-robocopy %wtfDir% "%tempdir%\WTF" /NFL /MIR /MT:32
+echo Creating temporary backup directory...
+mkdir %tempdir% || (echo "Failed to create temporary backup directory." && exit /b 1)
+echo.
+echo Copying Interface directory to temporary backup directory...
+robocopy %interfaceDir% "%tempdir%\Interface" /NFL /MIR /MT:32 || (echo "Failed to copy Interface directory." && exit /b 1)
+echo.
+echo Copying WTF directory to temporary backup directory...
+robocopy %wtfDir% "%tempdir%\WTF" /NFL /MIR /MT:32 || (echo "Failed to copy WTF directory." && exit /b 1)
 
 CD /D "C:\Program Files\7-Zip"
 
-7z.exe a -mmt=16 "%tempDir%.zip" "%tempDir%" -r
+echo.
+echo Compressing temporary backup directory...
+7z.exe a -mmt=16 "%tempDir%.zip" "%tempDir%" -r || (echo "Failed to compress temporary backup directory." && exit /b 1)
 
-rmdir /s /q %tempdir%
+echo.
+echo Removing temporary backup directory...
+rmdir /s /q %tempdir% || (echo "Failed to remove temporary backup directory." && exit /b 1)
 
-robocopy "D:\WoWBackUp" "%destDir%" /NFL /COPY:DAT /MT:32 /R:1 /W:1 /NP
+echo.
+echo Copying backup to destination directory...
+robocopy "D:\WoWBackUp" "%destDir%" /NFL /COPY:DAT /MT:32 /R:1 /W:1 /NP || (echo "Failed to copy backup to destination directory." && exit /b 1)
+
+echo.
+echo Backup process completed successfully.
+pause
+exit /b 0
--- update-wow-addons.bat
+++ update-wow-addons.bat
@@ -3,6 +3,7 @@ set "backupDir=T:\WoWAddonBackup"
 set "wowDir=C:\World of Warcraft\_retail_"
 set "sevenZipPath=C:\Program Files\7-Zip\7z.exe"
 
+echo Finding latest backup file...
 for /f "delims=" %%a in ('dir /b /o-d "%backupDir%"') do (
     set "latest=%%a"
     goto :foundLatest
@@ -10,21 +11,26 @@ for /f "delims=" %%a in ('dir /b /o-d "%backupDir%"') do (
 :foundLatest
 set "latestWithoutZip=%latest:.zip=%"
 
-echo %latestWithoutZip%
+echo Latest backup file: %latestWithoutZip%
 
+echo Copying backup to WoW directory...
 xcopy /s /e /y "%backupDir%\%latest%" "%wowDir%" || (echo "xcopy failed." && exit /b 1)
 
+echo Extracting backup...
 "%sevenZipPath%" x "%wowDir%\%latest%" -o"%wowDir%" -aoa || (echo "7z extraction failed." && exit /b 1)
 
+echo Deleting extracted zip file...
 del /q "%wowDir%\%latest%" || (echo "del failed." && exit /b 1)
 
+echo Removing Interface and WTF directories...
 rd /s /q "%wowDir%\WTF"
 rd /s /q "%wowDir%\Interface"
 
+echo Copying addons to WoW directory...
 xcopy /s /e /y "%wowDir%\%latestWithoutZip%\*" "%wowDir%" || (echo "xcopy failed." && exit /b 1)
 
+echo Cleaning up...
 rd /s /q "%wowDir%\%latestWithoutZip%" || (echo "del failed." && exit /b 1)
 
+echo Update completed successfully.
 exit /b 0
-
-pause
